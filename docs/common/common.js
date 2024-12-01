ac42b9443184476eb91ccfac063f966222ea0676
jahn-doe <trash-0-removable@outlook.jp>
2024-12-02 02:56:42 +0900

    サイト化実験

---
+++ docs/common/common.js
@@ -0,0 +1,25 @@
+(() => {
+    u('a#upper').handle('click', function(e) {
+        window.scroll({top: 0, behavior: 'smooth'});
+    });
+    u('article.tweet > blockquote > a').handle('click', function(e) {});
+    // ---
+    if(0 < u('img.psc').length) {
+        const $div = u('<div>').attr('id', 'dialog').addClass('hidden');
+        const $article = u('<article>').handle('click', function(e) {
+            $div.addClass('hidden');
+            u('body').removeClass('modal');
+        });
+        const $img = u('<img>').attr('id', 'dialog_img');
+        $article.append($img);
+        $div.append($article);
+        u('body').append($div);
+        // event
+        u('img.psc').handle('click', function(e) {
+            let path = u(this).attr('src');
+            $img.attr('src', path);
+            $div.removeClass('hidden');
+            u('body').addClass('modal');
+        });
+    }
+})();
